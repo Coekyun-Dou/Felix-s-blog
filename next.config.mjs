7e04313df2278743cc75e348fc640520126f72b1
less_duuuzx@163.com <Less_duuuzx@163.com>
2025-11-27 22:51:56 +0800

    优化界面

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,14 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com'
       }
     ],
   },
